802a8faee4ba4844fdd50576f7eb03122b026112
Sunnit-M <edwinnoblemessinger@icloud.com>
2026-02-03 15:24:09 -0800

    Added File Save on Stop Event Register

--- src/main/java/net/justsunnit/fern/Events/EventRegistry.java
+++ src/main/java/net/justsunnit/fern/Events/EventRegistry.java
@@ -1,9 +1,11 @@
 package net.justsunnit.fern.Events;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
 public class EventRegistry {
     public static void registerEvents() {
         ServerPlayConnectionEvents.JOIN.register(PlayerJoinEvent::run);
+        ServerLifecycleEvents.SERVER_STOPPING.register(ServerCycleEvents::Stopped);
     }
 }
